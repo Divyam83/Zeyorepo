98a564f57497925a4795be9fb61f48e77ea4ab76
divya <divya@LAPTOP-E27J7N2N.fios-router.home>
2021-06-24 23:49:37 -0400

    spark git object created

--- SparkGit/src/main/scala/Sparkpack/SparkgitObj.scala
+++ SparkGit/src/main/scala/Sparkpack/SparkgitObj.scala
@@ -1,5 +1,24 @@
 package Sparkpack
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+import sys.process._
 
 object SparkgitObj {
-  
+  def main(args: Array[String]): Unit = {
+
+    println("Hello World")
+
+    val conf = new SparkConf().setAppName("ES").setMaster("local[*]")
+    val sc = new SparkContext(conf)
+    sc.setLogLevel("ERROR")
+
+    val data = sc.textFile("file:///home/cloudera/data/txns")
+
+    val gymdata = data.filter(x => x.contains("Gymnastics"))
+
+    "hadoop fs -rmr /user/cloudera/gymdata_dir" !
+
+    gymdata.saveAsTextFile("hdfs:/user/cloudera/gymdata_dir")
+
+  }
 }
